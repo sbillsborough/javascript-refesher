7eaff79212d093ad9e365d034643d9357f60e175
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-02-19 16:17:02 +0000

    Use as keyword to asign an alias asigned abc the alias content with the as keyword

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,13 +4,17 @@ import { apiKey, abc } from "./util.js";
 // default value import, define a name now and still define the path
 import defaultApiKey from "./util.js";
 
-// multiple imports grouped together as a JavaScript object (any name, this example is named util)
-import * as util from "./util.js";
-
 console.log("This is the imported API key: " + apiKey);
 console.log(abc);
 console.log(`This is the default import API key: ${defaultApiKey}`);
 
+// multiple imports grouped together as a JavaScript object (any name, this example is named util)
+import * as util from "./util.js";
+
 // multiple imports grouped together as a JavaScript object, console logged with dot notation
 console.log("multiple imports console log: " + util.abc);
 console.log("Default import: " + util.default);
+
+// as keyword to asign an alias
+import { abc as content } from "./util.js";
+console.log("abc with alias content: " + content);
