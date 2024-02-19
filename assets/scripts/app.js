bf6d032f15549fdd20a8c2095cd8025b9d8f9c46
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-02-19 16:06:43 +0000

    Add multiple imports grouped together as an object
    added multiple imports as a JS object
    output with util.abc
    variable name, dot notation and exported variable name^^^^

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,15 @@
-import { apiKey } from "./util.js";
+// comma separate dlist for multiple imports
+import { apiKey, abc } from "./util.js";
 
 // default value import, define a name now and still define the path
-
 import defaultApiKey from "./util.js";
 
-console.log("Hello World!");
+// multiple imports grouped together as a JavaScript object (any name, this example is named util)
+import * as util from "./util.js";
+
 console.log("This is the imported API key: " + apiKey);
+console.log(abc);
 console.log(`This is the default API key: ${defaultApiKey}`);
+
+// multiple imports grouped together as a JavaScript object, console logged with dot notation
+console.log("multiple imports console log: " + util.abc);
